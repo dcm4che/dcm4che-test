482d6a1950de7e964dad064d7c35726d850a7352
Hesham Elbadawi <bsdreko@gmail.com>
2015-02-25 14:45:51 +0100

    [DCMEEREQ-246][LIB-306] Create remaining test tools
    Add secure tests

--- dcm4che-test-integration/src/test/java/org/dcm4che/test/samples/SampleTest.java
+++ dcm4che-test-integration/src/test/java/org/dcm4che/test/samples/SampleTest.java
@@ -4,6 +4,11 @@ import java.io.IOException;
 import java.security.GeneralSecurityException;
 
 import org.dcm4che.test.annotations.DcmGenParameters;
+import org.dcm4che.test.annotations.MppsParameters;
+import org.dcm4che.test.annotations.QueryParameters;
+import org.dcm4che.test.annotations.RemoteConnectionParameters;
+import org.dcm4che.test.annotations.StgCmtParameters;
+import org.dcm4che.test.annotations.StoreParameters;
 import org.dcm4che.test.common.BasicTest;
 import org.dcm4che.test.common.TestToolFactory;
 import org.dcm4che.test.common.TestToolFactory.TestToolType;
@@ -13,6 +18,8 @@ import org.dcm4che3.data.VR;
 import org.dcm4che3.net.IncompatibleConnectionException;
 import org.dcm4che3.tool.dcmgen.test.DcmGenTool;
 import org.dcm4che3.tool.findscu.test.QueryResult;
+import org.dcm4che3.tool.mppsscu.test.MppsResult;
+import org.dcm4che3.tool.stgcmtscu.test.StgCmtTool;
 import org.dcm4che3.tool.storescu.test.StoreResult;
 import org.junit.Assert;
 import org.junit.Test;
@@ -20,7 +27,9 @@ import org.junit.Test;
 public class SampleTest extends BasicTest {
 
     @Test
-    public void testSendDicom() throws IOException, InterruptedException, IncompatibleConnectionException, GeneralSecurityException {
+    @RemoteConnectionParameters(hostName="localhost",port=2762)
+    @StoreParameters(aeTitle="DCM4CHEE", baseDirectory="/pacsworld_Share_SlaveVM/Dicom-IHE-Server-Platform/Mesa_12_5_0/", connection="dicom-tls", sourceAETitle="STORESCU", sourceDevice="storescu")
+    public void testaSendDicom() throws IOException, InterruptedException, IncompatibleConnectionException, GeneralSecurityException {
         StoreResult result =  (StoreResult) storeResource("test description", "test.dcm");
         Assert.assertEquals(result.getFailures(), 0);
     }
@@ -31,7 +40,9 @@ public class SampleTest extends BasicTest {
     }
 
     @Test
-    public void testQueryNoFuzzyDicomDefaultRemoteConfig() throws Exception {
+    @RemoteConnectionParameters(hostName="localhost",port=2762)
+    @QueryParameters(aeTitle="DCM4CHEE", connection="dicom-tls", sourceAETitle="FINDSCU", sourceDevice="findscu")
+    public void testbQueryNoFuzzyDicomDefaultRemoteConfig() throws Exception {
         Attributes queryKeys = new Attributes();
 
         queryKeys.setString(Tag.PatientID, VR.LO, "0002");
@@ -40,6 +51,21 @@ public class SampleTest extends BasicTest {
         QueryResult result = (QueryResult) query("Query test Patient ID with Issuer", queryKeys, false);
         Assert.assertEquals(1, result.getNumMatches());
         System.out.println(result.getQueryResponse().toString());
+    }
+    @Test
+    @RemoteConnectionParameters(hostName="localhost", port=2762)
+    @MppsParameters(aeTitle="DCM4CHEE", connection="dicom-tls", sourceAETitle="MPPSSCU", sourceDevice="mppsscu", baseDirectory="/pacsworld_Share_SlaveVM/Dicom-IHE-Server-Platform/Mesa_12_5_0/")
+    public void testMPPS(){
+        MppsResult mppsResult = (MppsResult) mpps("Send MPPS for Study",
+                "modality/MR/MR4/MR4S1");
+    }
+    @Test
+//    @RemoteConnectionParameters(hostName="localhost", port=2762)
+//    @StgCmtParameters(aeTitle="DCM4CHEE", connection="dicom-tls", sourceAETitle="STGCMTSCU", sourceDevice="stgcmtscu", baseDirectory="/pacsworld_Share_SlaveVM/Dicom-IHE-Server-Platform/Mesa_12_5_0/",storageDirectory="/tmp")
+    //connection matching problem, to make this secure test work remove the configuration for the dicom connection from the archive and keep dicom-tls connection for STGCMTSCU
+    public void testStgCmt() throws InterruptedException, IOException, GeneralSecurityException, IncompatibleConnectionException{
+        StgCmtTool stgCmtTool = (StgCmtTool) TestToolFactory.createToolForTest(TestToolFactory.TestToolType.StorageCommitmentTool, this);
+        stgCmtTool.stgcmt("Storage Commitment MESA_MR_MR1_MRS1","modality/MR/MR1/MR1S1/MR1S1IM1.dcm");
     }
     @DcmGenParameters(instanceCount=1,outputDir="/testnew",seedFile="/test.dcm",seriesCount=1)
     @Test
