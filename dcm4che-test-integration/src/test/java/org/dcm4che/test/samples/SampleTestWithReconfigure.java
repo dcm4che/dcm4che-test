fd1a941f4a2e50b224f2b11947080188a7a1df14
romank-agfa <roman.khazankin@agfa.com>
2015-03-09 20:41:08 +0100

    [DCMEEREQ-256] Provide mechanisms to override server configuration | sample test

---
+++ dcm4che-test-integration/src/test/java/org/dcm4che/test/samples/SampleTestWithReconfigure.java
@@ -0,0 +1,55 @@
+package org.dcm4che.test.samples;
+
+import org.junit.Assert;
+import junit.framework.AssertionFailedError;
+import org.dcm4che.test.annotations.QueryParameters;
+import org.dcm4che.test.common.BasicTest;
+import org.dcm4che.test.common.TestToolException;
+import org.dcm4che.test.common.TestToolFactory;
+import org.dcm4che.test.integration.query.QueryTestSuite;
+import org.dcm4che3.data.Attributes;
+import org.dcm4che3.data.Tag;
+import org.dcm4che3.data.VR;
+import org.dcm4che3.net.ApplicationEntity;
+import org.dcm4che3.net.Device;
+import org.dcm4che3.tool.common.test.TestResult;
+import org.dcm4che3.tool.findscu.test.QueryResult;
+import org.dcm4che3.tool.findscu.test.QueryTool;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.io.IOException;
+
+/**
+ * @author Roman K
+ */
+public class SampleTestWithReconfigure extends BasicTest{
+
+
+    @Test
+    @QueryParameters(aeTitle = "DCM4CHEE")
+    public void testAERenaming() throws Exception {
+
+        // try to query existing AE - everything should be fine
+        query("", new Attributes(), false);
+
+        // rename the AE on the server
+        Device device = getRemoteConfig().findDevice("dcm4chee-arc");
+        device.getApplicationEntity("DCM4CHEE").setAETitle("DCM4CHEE-CHANGED");
+        getRemoteConfig().merge(device);
+
+        // try to query the non-existing AE
+        try {
+            query("", new Attributes(), false);
+            throw new AssertionFailedError("Query should have failed because of non existing AE title");
+        } catch (TestToolException e) {
+            Assert.assertTrue(e.getCause() instanceof IOException);
+            // it ok, noop
+        }
+
+        // rename the AE back
+        device.getApplicationEntity("DCM4CHEE-CHANGED").setAETitle("DCM4CHEE");
+        getRemoteConfig().merge(device);
+
+    }
+}
